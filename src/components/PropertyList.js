8cabf0154278f5e672b5e6c13db4a3f374db730f
jafar <jafaralihajari@gmail.com>
2025-01-19 12:10:16 +0530

    image issue solved

--- src/components/PropertyList.js
+++ src/components/PropertyList.js
@@ -105,7 +105,7 @@ const PropertyList = () => {
                 {property.images.map((image, index) => (
                   <img
                     key={index}
-                    src={`${process.env.REACT_APP_API_URL}/${image}`}
+                    src={`${process.env.REACT_APP_IMAGE_URL}/${image}`}
                     alt=""
                     className="property-image"
                   />
